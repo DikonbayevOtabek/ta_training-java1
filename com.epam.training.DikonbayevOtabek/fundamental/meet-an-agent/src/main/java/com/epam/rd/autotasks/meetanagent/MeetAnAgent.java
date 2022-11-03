6cfa43a34c43ab121ec0ae5a2298e9a875305a61
Dikonbayev Otabek <dikonbayev@gmail.com>
2022-11-03 18:02:58 +0500

    add: meet-an-agent project

---
+++ com.epam.training.DikonbayevOtabek/fundamental/meet-an-agent/src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
@@ -0,0 +1,18 @@
+package com.epam.rd.autotasks.meetanagent;
+
+import java.util.Scanner;
+
+public class MeetAnAgent {
+    final static int PASSWORD = 133976; //You can change pass, but don't change the type
+
+    public static void main(String[] args) {
+        //put you code here
+        Scanner scan = new Scanner(System.in);
+        int password = scan.nextInt();
+        if (password == PASSWORD){
+            System.out.println("Hello, Agent");
+        }else{
+            System.out.println("Access denied");
+        }
+    }
+}
