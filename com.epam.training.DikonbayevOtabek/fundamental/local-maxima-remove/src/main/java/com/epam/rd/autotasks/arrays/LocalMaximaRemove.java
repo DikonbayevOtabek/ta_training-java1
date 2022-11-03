2aa1a1f584cd9f1392e3ff482f68d6f5750ca6e8
Dikonbayev Otabek <dikonbayev@gmail.com>
2022-11-03 16:26:50 +0500

    add: local-maxima-remove project

---
+++ com.epam.training.DikonbayevOtabek/fundamental/local-maxima-remove/src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -0,0 +1,34 @@
+package com.epam.rd.autotasks.arrays;
+
+import java.util.Arrays;
+
+public class LocalMaximaRemove {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{18, 1, 3, 6, 7, -5};
+
+        System.out.println(Arrays.toString(removeLocalMaxima(array)));
+    }
+
+    public static int[] removeLocalMaxima(int[] array){
+
+        //put your code here
+        int [] result = new int[array.length];
+        int j = 0;
+        for (int i = 0; i < array.length; ++i){
+            if (i == 0 && array[i] > array[i + 1]){
+                continue;
+            }else if (i != array.length - 1 && i != 0 && array[i] > array[i - 1] && array[i] > array[i + 1]){
+                continue;
+            }else if (i == array.length - 1 && array[i] > array[i - 1]){
+                continue;
+            }else{
+                result[j] = array[i];
+                ++j;
+            }
+        }
+        int [] res = Arrays.copyOf(result, j);
+        return res;
+
+    }
+}
