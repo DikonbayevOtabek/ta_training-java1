b1a0e083d5b978e66ec6ed83e8066c583e75cc8d
Dikonbayev Otabek <dikonbayev@gmail.com>
2022-11-03 16:05:13 +0500

    add: decrementing-carousel-with-limit project

---
+++ com.epam.training.DikonbayevOtabek/fundamental/decrementing-carousel-with-limit/src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -0,0 +1,64 @@
+package com.epam.rd.autotasks;
+
+public class DecrementingCarousel {
+    int capacity, i = 0, run = 0;
+    int [] arr;
+
+    public int getFlag(){
+        return run;
+    }
+    public void setFlag(int num){
+        run = num;
+    }
+
+    public int[] getArr() {
+        int [] result = new int[i];
+        run = 1;
+        if (capacity > 0){
+            for (int j = 0; j < arr.length; ++j){
+                if (arr[j] > 0){
+                    result[j] = arr[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public DecrementingCarousel(int capacity) {
+        this.capacity = capacity;
+        arr = new int[capacity];
+    }
+
+    public boolean addElement(int element){
+        if (element <= 0){
+            return false;
+        }else if (i >= arr.length){
+            return false;
+        }else if (run == 1){
+            return false;
+        }else{
+            arr[i] = element;
+            ++i;
+            return true;
+        }
+    }
+
+    public CarouselRun run(){
+        if (run == 0 ){
+            int [] result = new int[i];
+            run = 1;
+            if (capacity > 0){
+                for (int j = 0; j < arr.length; ++j){
+                    if (arr[j] > 0){
+                        result[j] = arr[j];
+                    }
+                }
+            }
+            return new CarouselRun(result);
+        }else{
+            return null;
+        }
+
+    }
+
+}
